2c12d1ea4e6a807b2e54a50bb25ed0fc98cc33f0
Stephen Cowen <scowen@email.arizona.edu>
2023-07-05 14:26:58 -0400

    Update odor_solenoid_control.ino

--- Arduino_code/odor_solenoid_control/odor_solenoid_control.ino
+++ Arduino_code/odor_solenoid_control/odor_solenoid_control.ino
@@ -1,27 +1,29 @@
+//////////////////////////////////////////////////////
+// Code for delivering a sequence of odors to the headfixed mouse
+// Cowen and NS&B Students 2023
+//////////////////////////////////////////////////////
 int n_solenoids = 5;
-byte solenoid_pins[] = {9, 10, 11, 12, 13};
+byte solenoid_pins[] = {9, 10, 11, 12, 13}; // so far, pin/odor port 9 does not work
+int port_open_time_ms = 200;
 
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  Serial.print("STARTED");
+  Serial.begin(9600);
+  Serial.println("STARTED");
   for (int i = 0;i<n_solenoids;i++){
     pinMode(solenoid_pins[i], OUTPUT);
     digitalWrite(solenoid_pins[i], LOW);
-
   }
 }
 
 // the loop function runs over and over again forever
 void loop() {
-
   for (int i = 0;i<n_solenoids;i++){
     Serial.print("Sol ");
-    Serial.print(solenoid_pins[i]);
-    Serial.print(",");
+    Serial.println(solenoid_pins[i]);
     digitalWrite(solenoid_pins[i], HIGH);
-    delay(1000);                      // wait for a second
+    delay(port_open_time_ms);                      // wait 
     digitalWrite(solenoid_pins[i], LOW);
-    delay(50);                      // wait for a second
-
+    delay(50);                      // wait 
   }
 }
