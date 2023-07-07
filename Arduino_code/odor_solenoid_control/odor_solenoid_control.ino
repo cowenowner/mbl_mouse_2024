28dc35363ea1652e92705302a6067541f1e4d217
Stephen Cowen <scowen@email.arizona.edu>
2023-07-07 16:12:28 -0400

    Added code for TTL pulse

--- Arduino_code/odor_solenoid_control/odor_solenoid_control.ino
+++ Arduino_code/odor_solenoid_control/odor_solenoid_control.ino
@@ -4,6 +4,8 @@
 //////////////////////////////////////////////////////
 int n_solenoids = 37;
 byte solenoid_pins[] = {10, 11, 12, 13, 10, 11, 11, 12, 11, 12, 10, 12, 13, 12, 10, 10, 11, 13, 11, 12, 11, 13, 12, 10, 12, 13, 13, 13, 11, 10, 12, 10, 11, 12, 11, 12, 13}; // so far, pin/odor port 9 does not work
+byte ttl_odor_code_pin = 14;
+int ttl_time_ms = 5;  
 int port_open_time_ms = 1000;
 int port_closed_time_ms = 5000;
 
@@ -24,6 +26,9 @@ void loop() {
     Serial.print(i+1);
     Serial.print(", Pin ");
     Serial.println(solenoid_pins[i]);
+    digitalWrite(ttl_odor_code_pin, HIGH);
+    delay(ttl_time_ms);
+    digitalWrite(ttl_odor_code_pin, LOW);
     digitalWrite(solenoid_pins[i], HIGH);
     delay(port_open_time_ms);                      // wait 
     digitalWrite(solenoid_pins[i], LOW);
