fbfddda07a65f01cbba79e9803d97aaeee2f3987
Matthijs van der Meer <mvdm@dartmouth.edu>
2023-07-26 15:24:55 -0400

    WIP on odor decoding script

---
+++ student_and_instructor_code/mvdm/SCRIPT_odor_decoding.m
@@ -0,0 +1,60 @@
+%% depth profile for Neuropixels 2.0 data
+addpath(genpath('C:\Users\mvdmlab\Documents\GitHub\mbl_mouse_2023\vandermeerlab\code-matlab\shared'));
+
+cd('C:\data-temp\HC_2_Neuro2_g0\preprocessed');
+load AllSpikes;
+load odor_events;
+
+% make spikes ts -- should convert into function
+nCells = length(SP);
+
+% sort by depth along probe
+depths = [SP(:).neuropixels_depth_uM];
+[~, sort_idx] = sort(depths, 'descend');
+SP = SP(sort_idx);
+
+myS = ts;
+for iC = nCells:-1:1
+
+    SP(iC).t = SP(iC).t_uS * 10^-6;
+    S.t{iC} = SP(iC).t;
+    S.label{iC} = SP(iC).cluster_id;
+
+end
+clear SP;
+
+
+%% MUA-peth
+
+cfg_Q = []; cfg_Q.smooth = 'gauss'; cfg_Q.gausswin_sd = 0.05;
+Q = MakeQfromS(cfg_Q, S);
+
+MUA = Q;
+MUA = zscore_tsd(MUA);
+MUA.data = nanmean(MUA.data);
+
+for iC = 1:3
+    cue_evt(iC) = SelectTS([], evt, iC); % select events for each individual cue
+end
+
+cfg_peth = []; % parameters for PETH
+cfg_peth.window = [-1 1];
+cfg_peth.dt = 0.01;
+cfg_peth.mode = 'interp';
+
+figure;
+
+cols = 'rgb';
+for iC = 1:3
+    out = TSDpeth(cfg_peth, MUA, cue_evt(iC));
+    h(iC) = plot(out, 'Color', cols(iC), 'LineWidth', 2);
+    hold on;
+end
+
+set(gca, 'FontSize', 18, 'TickDir', 'out'); box off;
+legend(h, {'Cue 1', 'Cue 2', 'Cue 3'}); legend boxoff;
+
+xlabel('Time from cue onset (s)')
+ylabel('z-scored MUA')
+
+vline(0,':');
